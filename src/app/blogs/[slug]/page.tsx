7f55509199d3f5a43a702cddd03206bc3331ad38
kara sealeaf <kara.sealeaf13@gmail.com>
2023-11-23 11:27:03 +0000

    blog added

--- src/app/blogs/[slug]/page.tsx
+++ src/app/blogs/[slug]/page.tsx
@@ -1,10 +1,24 @@
-import Image from 'next/image'
+import Image from "next/image";
+import { getPostsBySlug } from "@/lib/posts";
+import { notFound } from "next/navigation";
 
-export default function Page() {
+type blogPostParams = {
+  params: {
+    slug: string;
+  };
+};
+
+export default function Page({ params }: blogPostParams) {
+  const post = getPostsBySlug(params.slug);
+
+  if (!post) {
+    notFound();
+  }
+
   return (
-    <div className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>This is the post title. </h1>
-      <p>This is the post content</p>
+    <div>
+      <h1>{post.title}</h1>
+      <p>{post.content}</p>
     </div>
-  )
+  );
 }
