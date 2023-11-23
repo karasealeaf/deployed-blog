f9e743c81f7d14f76a53533730792cdaa0d2947f
kara sealeaf <kara.sealeaf13@gmail.com>
2023-11-23 09:22:31 +0000

    init bruv

---
+++ src/app/blogs/[slug]/page.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+
+export default function Page() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>This is the post title. </h1>
+      <p>This is the post content</p>
+    </div>
+  )
+}
